1907adb0538085da8ee44e6152d2a19f54626dff
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2024-01-12 13:06:16 -0300

    Update route.rs

--- src/route.rs
+++ src/route.rs
@@ -1,1 +1,18 @@
+// Route definitions
 
+use yew_router::prelude::*;
+use yew::prelude::*;
+
+use crate::pages::home::Home;
+
+#[derive(Clone, Routable, PartialEq)]
+pub enum Route {
+    #[at("/")]
+    Home,
+}
+
+pub fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => { html! { <Home /> }},
+    }
+}
